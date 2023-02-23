18279c0767c5fa53521565c03b8ef8f27ed4d0bf
sai-darshk <44713917+sai-darshk@users.noreply.github.com>
2023-02-23 09:46:59 -0600

    Update app.js

--- app.js
+++ app.js
@@ -11,3 +11,26 @@ const observer = new IntersectionObserver ((entries) => {
 
   const hiddenElements = document.querySelectorAll('.hidden') ;
   hiddenElements.forEach((el) => observer.observe (el));
+
+  const navLinks = document.querySelectorAll('nav ul li a');
+
+for (const link of navLinks) {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href');
+    const targetSection = document.querySelector(targetId);
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+
+  });
+}
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+
+
+
+
