27a84c12905fdbd12d3883e80fc493c2f80e9b6a
sai-darshk <44713917+sai-darshk@users.noreply.github.com>
2023-02-23 21:49:56 -0600

    Update app.js

--- app.js
+++ app.js
@@ -24,12 +24,25 @@ for (const link of navLinks) {
   });
 }
 
-function scrollToTop() {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-}
+const scrollToTopBtn = document.getElementById("scroll-to-top-btn");
+
+window.addEventListener("scroll", () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+});
+
+scrollToTopBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
+
+
+
+
+
 
 
 
